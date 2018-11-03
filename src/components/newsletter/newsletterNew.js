b8875fd2ee35243cc4536d1f7ed1d3f7d4e54152
patdevincentis <devincentis529@gmail.com>
2018-11-03 00:26:21 -0400

    built cancel button placed form buttons on grid

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -3,14 +3,29 @@ import NewNewsletterForm from './newsletterNewForm'
 
 class NewNewsletter extends Component {
 
-  onSubmit = (fields) => {
-    console.log('trying to handle submit')
+  onSubmit = (fields, button) => {
+
+    // if (button == 'submit') {
+    //   this.props.history.push('/dashboard');
+    // } else {
+    //   // save new newsletter on the backend and perform a post req
+    //   console.log('trying to submit backend');
+
+    // }
+    console.log('trying to submit')
+  }
+
+  onCancel = () => {
+
+    //   this.props.history.push('/dashboard');
+    console.log('trying to cancel')
+
   }
 
   render() {
     return (
       <div className='new-newsletter'>
-        <NewNewsletterForm onSubmit={(event) => this.onSubmit(event)} />
+        <NewNewsletterForm onCancel={() => this.onCancel()} onSubmit={(event) => this.onSubmit(event)} />
       </div>
     )
   }
--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -31,13 +31,25 @@ class NewNewsletterForm extends Component {
         />
 
         <Field
-          className="sign-in-form__submit"
+          className="new-newsletter-form__submit"
           small={true}
           danger={true}
           name="Submit"
           type="submit"
           title="Submit"
           component={FormButton}
+
+        />
+
+        <Field
+          className="new-newsletter-form__cancel"
+          small={true}
+          name="Cancel"
+          type="button"
+          title="Cancel"
+          component={FormButton}
+          onClick={this.props.onCancel}
+
         />
 
       </form>
