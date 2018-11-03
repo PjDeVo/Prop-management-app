cd14ec56a77ac3843ac545d70e1c730a1438ec61
patdevincentis <devincentis529@gmail.com>
2018-11-02 22:55:47 -0400

    customized newsletter new form

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NewNewsletterForm from './newsletterNewFrom'
+import NewNewsletterForm from './newsletterNewForm'
 
 class NewNewsletter extends Component {
 
