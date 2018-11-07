4abd90b8f1b7c6b6e1e3758e635f5f193fa02ca0
patdevincentis <devincentis529@gmail.com>
2018-11-07 17:51:55 -0500

    built grid for requests items

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -21,9 +21,20 @@ class RequestsItem extends Component {
         <Icon className='requests-item__arrow' icon='fas fa-sort-down' />
         <div className='requests-item__date'>
           05/29/1995
-  
+
         </div>
         <Button icon='fas fa-wrench' className='requests-item__move' callback={() => console.log('trying to move request item/change jawns status')} />
+        <div className='requests-item__description'>
+          <img
+            className='requests-item__description'
+            src='http://via.placeholder.com/160x94'
+
+          />
+          <p className='requests-item__description-text' />
+          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        </div>
       </div>
     )
   }
