0574b8584912b0016b5a8608db72387d2b99b052
patdevincentis <devincentis529@gmail.com>
2018-11-07 17:07:43 -0500

    built an icon function and created file solely for icon on requests page

---
+++ src/components/requests/requestsItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Icon from '../icon'
+
+
+class RequestsItem extends Component {
+
+
+  render() {
+    return (
+      <div className='requests-item'>
+        <div>
+          <Icon icon='fas fa-exclamation-triangle' />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RequestsItem;
