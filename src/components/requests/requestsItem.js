b5d370ae78a24fc5495beea4affb8d2f58499df4
patdevincentis <devincentis529@gmail.com>
2018-11-08 12:38:01 -0500

    laid out JSX on requests items Grid

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
-import Icon from '../icon';
-import Button from '../button'
-
+import React, { Component } from "react";
+import Icon from "../icon";
+import Button from "../button";
 
 class RequestsItem extends Component {
-
-
   render() {
     return (
-      <div className='requests-item'>
-        <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle' />
-        <div className='requests-item__title'>
-          Breh da Fuck
+      <div className="requests-item">
+        <Icon
+          className="requests-item__icon"
+          icon="fas fa-exclamation-triangle"
+        />
+        <div className="requests-item__title">Breh da Fuck</div>
 
-        </div>
-
-        <div className='requests-item__tenant-unit'>
-          PattyCakes - unit 115
-        </div>
-        <Icon className='requests-item__arrow' icon='fas fa-sort-down' />
-        <div className='requests-item__date'>
-          05/29/1995
-
-        </div>
-        <Button icon='fas fa-wrench' className='requests-item__move' callback={() => console.log('trying to move request item/change jawns status')} />
-        <div className='requests-item__description'>
+        <div className="requests-item__tenant-unit">PattyCakes - unit 115</div>
+        <Icon className="requests-item__arrow" icon="fas fa-sort-down" />
+        <div className="requests-item__date">05/29/1995</div>
+        <Button
+          icon="fas fa-wrench"
+          className="requests-item__move"
+          callback={() =>
+            console.log("trying to move request item/change jawns status")
+          }
+        />
+        <div className="requests-item__description">
           <img
-            className='requests-item__description'
-            src='http://via.placeholder.com/160x94'
-
+            className="requests-item__description-img"
+            src="http://via.placeholder.com/160x94"
           />
-          <p className='requests-item__description-text' />
-          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          <p className="requests-item__description-text" />
+          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+          Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          Lorem Ipsum
         </div>
       </div>
-    )
+    );
   }
 }
 
