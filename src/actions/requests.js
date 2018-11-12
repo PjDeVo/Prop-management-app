5ec4d02a7a2d408692cc2bde7b5a007d6f555742
patdevincentis <devincentis529@gmail.com>
2018-11-12 16:09:36 -0500

    built creatge new request action creator with redux-thunk middlware

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -1,14 +1,23 @@
-import {
-  CHANGE_SELECTED_REQUEST_TYPE
+import { CHANGE_SELECTED_REQUEST_TYPE } from "./types";
 
-} from './types';
+import axios from "axios";
+import { ROOT_URL } from "../config";
 
 export function changeSelectedRequestType(boxType) {
+  return {
+    type: CHANGE_SELECTED_REQUEST_TYPE,
+    payload: boxType
+  };
+}
 
-  return (
-    {
-      type: CHANGE_SELECTED_REQUEST_TYPE,
-      payload: boxType
-    }
-  )
-};  
+export function createNewRequest(newRequest, success) {
+  return function() {
+    axios.post(`${ROOT_URL}/requests/new`, newRequest);
+    then(response => {
+      console.log(response.data);
+      success();
+    }).catch(err => {
+      console.log(err);
+    });
+  };
+}
