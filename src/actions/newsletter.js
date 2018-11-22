12705afdfbd90f8c8d7180b78e963d79af2ab95e
patdevincentis <devincentis529@gmail.com>
2018-11-21 22:10:30 -0500

    refactured fetch newsletter to actually getch from the backend

--- src/actions/newsletter.js
+++ src/actions/newsletter.js
@@ -1,40 +1,28 @@
-import {
-  SET_NEWSLETTERS,
-  FETCH_NEWSLETTER_ID
-} from './types';
+import { SET_NEWSLETTERS, FETCH_NEWSLETTER_ID } from "./types";
 
+import axios from "axios";
 
 export function fetchNewsletters() {
-
-  const response = {
-    data: [
-      {
-        _id: '23',
-        title: 'Happy Holidays Fam',
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        date: new Date(),
-        imageUrl: 'http://via.placeholder.com/960x258'
-      },
-
-      {
-        _id: '24',
-        title: 'Second Jawn',
-        body: "Lorem IpsBREEEHH ing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        date: new Date(),
-        imageUrl: 'http://via.placeholder.com/960x258'
-      }
-
-    ]
-  }
-  return {
-    type: SET_NEWSLETTERS,
-    payload: response.data
-  }
+  return function distpatch() {
+    const token = localStorage.getItem("token");
+    const headers = { headers: { authorization: token } };
+    axios
+      .get(`${ROOT_URL}/newsletters`, headers)
+      .then(response => {
+        dispatch({
+          type: SET_NEWSLETTERS,
+          payload: response.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 }
 
 export function fetchNewsletterWithId(id) {
   return {
     type: FETCH_NEWSLETTER_ID,
     payload: id
-  }
+  };
 }
--- src/components/requests/requestsBox.js
+++ src/components/requests/requestsBox.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
-import * as actions from '../../actions';
-import { connect } from 'react-redux';
-
-
+import React, { Component } from "react";
+import * as actions from "../../actions";
+import { connect } from "react-redux";
 
 class RequestsBox extends Component {
   render() {
-    const { count, title } = this.props;
-    const className = `requests-box ${this.props.selectedRequestType == title ? 'requests-box-active' : 'requests-box-inactive'}`
+    const { count, title, icon } = this.props;
+    const className = `requests-box ${
+      this.props.selectedRequestType == title
+        ? "requests-box-active"
+        : "requests-box-inactive"
+    }`;
     return (
-      <a onClick={() => this.props.changeSelectedRequestType(title)} className={className}>
-        <div className='requests-box__count'> {count}</div>
-        <div className='requests-box__title'>{title}</div>
-        <div className='requests-box__point'></div>
+      <a
+        onClick={() => this.props.changeSelectedRequestType(title)}
+        className={className}
+      >
+        <div className="requests-box__count"> {count}</div>
+        <div className="requests-box__title">{title}</div>
+        <div className="requests-box__point" />
+        <div className={`${icon} requests-box__icon`} />
       </a>
-    )
+    );
   }
 }
 function mapStateToProps(state) {
-  const { selectedRequestType } = state.requests
+  const { selectedRequestType } = state.requests;
   return { selectedRequestType };
 }
 
-RequestsBox = connect(mapStateToProps, actions)(RequestsBox);
+RequestsBox = connect(
+  mapStateToProps,
+  actions
+)(RequestsBox);
 
 export default RequestsBox;
