4c5a0544c81bd5302f1bd541e9a8642f3199396c
patdevincentis <devincentis529@gmail.com>
2018-11-26 17:26:20 -0500

    hooked up layout to mapstate to props

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,12 @@
-import React from 'react';
-export function Header() {
+import React from "react";
+export function Header({ title, subtitle }) {
   return (
-    <div className='header'>
-      <h1 className='header__title'>Welcome to HOA Manager!</h1>
-      <p className='header__subtitle'>Please login to continue</p>
+    <div className="header">
+      <h1 className="header__title">{title}</h1>
+      <p className="header__subtitle">{subtitle}</p>
     </div>
-  )
+  );
 }
 export function HeaderBar() {
-  return (
-    <div className='bar'></div>
-  )
+  return <div className="bar" />;
 }
-
-
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
-import { Header, HeaderBar } from './header';
-
+import React, { Component } from "react";
+import { Header, HeaderBar } from "./header";
 
 class Layout extends Component {
   render() {
     return (
-      <div className='layout-grid'>
-        <Header />
+      <div className="layout-grid">
+        <Header
+          title="Welcome to HOA Manager!"
+          subtitle="Please login to continue"
+        />
         <HeaderBar />
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
 export default Layout;
-
-
-
-
-
