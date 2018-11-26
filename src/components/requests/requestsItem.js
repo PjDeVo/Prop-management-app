7b595112b74ed9bc738f6c3947b60bef654d00cb
patdevincentis <devincentis529@gmail.com>
2018-11-26 17:17:10 -0500

    added unique id fore requests item ids for animations

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -20,7 +20,7 @@ class RequestsItem extends Component {
   }
 
   toggleDropdown = () => {
-    var element = document.getElementById("requests-item");
+    var element = document.getElementById(`${this.props._id}-requests-item`);
     if (this.state.height == 0) {
       element.classList.add("bg-F8");
       this.setState({ height: "auto" });
@@ -38,7 +38,7 @@ class RequestsItem extends Component {
   };
 
   render() {
-    const { title, body, date, imageUrl, status } = this.props;
+    const { title, body, date, imageUrl, status, _id } = this.props;
 
     const parsedDate = new Date(date);
 
@@ -53,7 +53,7 @@ class RequestsItem extends Component {
     }
 
     return (
-      <div id="requests-item" className="requests-item">
+      <div id={`${_id}-requests-item`} className="requests-item">
         <Icon className="requests-item__icon" icon={mainIcon} />
         <div className="requests-item__title">
           <div className="requests-item__title__text">{title}</div>
